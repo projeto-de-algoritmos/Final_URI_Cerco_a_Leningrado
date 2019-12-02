ec87d56919734b0a1634eddb7c4b28b126989b38
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-12-02 04:46:28 -0300

    Adição da solução da questão.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Final_URI_Cerco_a_Leningrado
+# Gerenciador de Tarefas 2.0
+
+**Número da Lista**: Lista 05<br>
+**Conteúdo da Disciplina**: Programação Dinâmica<br>
+
+## Alunos
+|Matrícula | Aluno |
+| -- | -- |
+| 15/0059329  |  Francisco Wallacy Coutinho Braz |
+| 17/0144259  |  Gustavo Nogueira Rodrigues |
+
+## Sobre 
+O objetivo do projeto é selecionar um conjunto máximo de tarefas compatíveis a partir das tarefas e dos pesos de cada tarefa(relevância) inseridas pelo usuário.
+
+**Algoritmo utilizado:**
+Weighted Job Scheduling.
+
+## Screenshots
+![](demonstracao.gif)
+
+## Instalação 
+**Linguagem**: JavaScript<br>
+O projeto pode ser executado a partir dos navegadores google chrome ou mozilla firefox. Passos para execução:
+1) Clone do projeto: git clone git@github.com:projeto-de-algoritmos/PD_Gerenciador_de_Tarefas_2.0.git
+2) Abrir o arquivo index.html pelo navegador.
+
+## Uso 
+1) O usuário inicialmente informa a quantidade de tarefas e a quantidade de horários disponíveis para realização das tarefas.
+2) Em seguida o usuário deve selecionar, a partir de uma tabela, os horários desejados para realização de cada tarefa, bem como o peso(relevância) de cada tarefa. 
+3) Por fim, o usuário deve clicar no botão 'Enviar' para que seja encontrado o conjunto máximo de tarefas compatíveis e assim é retornado na interface esse conjunto.
+
+
---
+++ cerco_leningrado.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ii = pair<int, int>;
+using pp = pair<int, ii>;
+
+const int MAX { 100010 }, oo { 1000010 };
+int dist[MAX];
+int w[MAX];
+vector<int> adj[MAX];
+bitset<MAX> processed;
+
+int dijkstra(int s,int dest ,int k,int N){
+ 	for (int i = 1; i <= N; ++i)
+ 		dist[i] = oo;
+
+ 	dist[s] = 0;
+ 	processed.reset();
+	priority_queue<pp, vector<pp>, greater<pp>> pq;
+    pq.push(pp(0,ii(s, k - w[s])));
+
+ 	while (not pq.empty()){
+ 		auto qq = pq.top();
+        auto u = qq.second.first;
+        auto b = qq.second.second;
+        auto d = qq.first;
+ 		pq.pop();
+
+ 		if (processed[u]) continue;
+
+ 		processed[u] = true;
+        for(auto v : adj[u]){
+ 			if ((dist[v] > d + w[v])&&(b - w[v] >= 0)) {
+ 				dist[v] = d + w[v];
+                pq.push(pp(dist[v],ii(v,b-w[v])));
+ 			}
+ 		}
+ 	}
+
+ 	if(dist[dest] == oo) return -1;
+ 	else return (dist[dest] + w[s]);
+}
+
+int main(){
+    int n,m,k,a,b,s,pos,o,d;
+    double p;
+
+    while(cin >> n >> m >> k >> p){
+        for(int i = 0;i < m;i++){
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        memset(w,0,sizeof(w));
+        cin >>  s;
+        while(s--){
+            cin >> pos;
+            w[pos]++;
+        }
+
+        cin >> o >> d;
+        int ret = dijkstra(o,d,k,n);
+        if(ret == -1) cout << "0.000" << endl;
+        else{
+            double r = pow(p,ret);
+            cout << fixed;
+            cout.precision(3);
+            cout << r << endl;
+        }
+        for(int i = 1;i <= n;i++) adj[i].clear();
+    }
+
+    return 0;
+}
